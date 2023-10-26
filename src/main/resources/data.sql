e63a0012d1e11095fdd1650c599c0f097b904d68
Camila Arredondo <cami.arredondo95@gmail.com>
2023-10-26 13:16:52 -0300

    cambios en el data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,10 @@
 insert into tipo_mascota (tipo_mascota) values ("Zorro Chilote");
 
-insert into nivel (nivel, experiencia) values (0 , 1);
-insert into nivel (nivel, experiencia) values (10 , 2);
-insert into nivel (nivel, experiencia) values (20 , 3);
-insert into nivel (nivel, experiencia) values (30 , 4);
-insert into nivel (nivel, experiencia) values (40 , 5);
+insert into nivel (nivel, experiencia) values (1 , 0);
+insert into nivel (nivel, experiencia) values (2 , 10);
+insert into nivel (nivel, experiencia) values (3 , 20);
+insert into nivel (nivel, experiencia) values (4 , 30);
+insert into nivel (nivel, experiencia) values (5 , 40);
 
 insert into tipo_mision (tipo_mision) values ("De Campo");
 insert into tipo_mision (tipo_mision) values ("Especiales");
